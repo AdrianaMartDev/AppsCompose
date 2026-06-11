1a549e8430be9fe2c37536cd43d93441ed9022df
amartinez <adriana.martinez.dev@gmail.com>
2026-06-11 14:24:57 -0600

    Create domain models for Category and Dish
    - Define `Category` data class with properties for category name and image
    - Define `Dish` data class with properties for dish name, description, price, and category association

---
+++ AdmRestaurant/app/src/main/java/com/example/admrestaurant/domain/model/Category.kt
@@ -0,0 +1,6 @@
+package com.example.admrestaurant.domain.model
+
+data class Category(
+    val nameCategory: String = "",
+    val imageCategory: String = ""
+)
---
+++ AdmRestaurant/app/src/main/java/com/example/admrestaurant/domain/model/Dish.kt
@@ -0,0 +1,8 @@
+package com.example.admrestaurant.domain.model
+
+data class Dish(
+    val nameDish: String = "",
+    val descDish: String = "",
+    val priceDish: Double = 0.0,
+    val category: String = ""
+)
